f95bbd00f67805f18f5e116e37656865211f96ff
bhaskaraccelyzei <bhaskar.sivaraj@accelyzei.com>
2025-06-30 12:47:29 +0530

    updates

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import Dashboard from '../pages/Dashboard'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/login' element={<LoginPage />} />
         <Route path='/dashboard' element={<Dashboard />} />
-        {/* <Route path='' element={<Navigate to="/login" />} /> */}
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='' element={<Navigate to="/dashboard" />} />
     </Routes>
   )
 }
