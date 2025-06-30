efbe02e671387a193c2f52074548788b359b39f5
bhaskaraccelyzei <bhaskar.sivaraj@accelyzei.com>
2025-06-30 12:51:09 +0530

    Rebuild for vercel routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppRoutes from './routes/AppRoutes';
 function App() {
   
   return (
-      <BrowserRouter basename="/task-delegation-app">
+      <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
   )
